ac5f0052baf35c32fb178fd781abda0be9a1660e
Gabor Major <gabor.major@csn.hu>
2013-06-24 01:42:32 +0200

    RubyGems 0.0.1 release of CircularMail

--- circular-mail/lib/circular-mail.rb
+++ circular-mail/lib/circular-mail.rb
@@ -1,12 +1,27 @@
 # encoding: UTF-8
 
-# Circular-Mail Library
+# Circular-Mail Library 0.0.1
 # =================================================================================================
 #      Author: gabor.major@csn.hu
 #      Github: https://github.com/goobemaster/ruby-not-magic/circular-mail/
 #    Rubygems: http://rubygems.org/gems/circular-mail
 # Description: Mass email sending through smtp. Highly configurable, feature rich, robust solution.
 #              Follows standards laid out in RFC-2822 (http://tools.ietf.org/html/rfc2822)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
 # =================================================================================================
 
 $LOAD_PATH << './'
